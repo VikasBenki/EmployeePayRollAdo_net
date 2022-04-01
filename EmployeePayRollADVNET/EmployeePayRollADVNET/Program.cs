9cf8e01c677c962671c34112a672e6e728c71c43
VikasBenki <vikasmsbenki@gmail.com>
2022-04-01 16:23:06 +0530

    [VikasBenki] welcome message

---
+++ EmployeePayRollADVNET/EmployeePayRollADVNET/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollADVNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello welcome to Ado.Net Program of Employepayroll");
+            Console.ReadLine();
+        }
+    }
+}
