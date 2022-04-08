6cae9671eda8d85c7ca7f33b9e193f754d7e0424
VikasBenki <vikasmsbenki@gmail.com>
2022-04-08 21:00:37 +0530

    [Vik] modified

--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -30,7 +30,7 @@ END CATCH
 exec spAddEmployee;
 
 --Update employee details in the database--
-create procedure spUpdateEmployee
+Create procedure spUpdateEmployee
 (
 @Id int,
 @Name varchar(100),
@@ -70,3 +70,12 @@ ERROR_LINE() AS ErrorLine,
 ERROR_MESSAGE() AS ErrorMessage;
 END CATCH
 select * from employee_payroll
+
+--Find Sum, Average, min, max, count of salary by gender--
+Create procedure spDBFunctions
+as
+select sum(Salary) as sumsalary,Gender from employee_payroll group by Gender;
+select avg(Salary) as avgsalary,Gender from employee_payroll group by Gender; 
+select max(Salary) as maxsalary,Gender from employee_payroll group by Gender; 
+select min(Salary) as minsalary,Gender from employee_payroll group by Gender; 
+select count(Name) as EmployeeCount,Gender from employee_payroll group by Gender; 
